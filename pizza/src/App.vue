8b1c64c8905b4c3e1410934007d4478c0e07c404
Arkhangelsky Andrey <an.anarxist@mail.ru>
2020-12-24 18:13:19 +0300

    Added pizza app

---
+++ pizza/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+   <h1>Parent: {{pizzaName}}</h1>
+   <app-pizza :pizzaName='pizzaName' :pizzaPrice='pizzaPrice' :imgURL='imgURL'>
+     <div slot="components" class="components_list">
+        <h2>Состав:</h2>
+        <ul>
+          <li>пицца</li>
+          <li>баги</li>
+          <li>костыли</li>
+          <li>любовь</li>
+        </ul>
+     </div>
+   </app-pizza>
+  </div>
+</template>
+
+<script>
+  import Pizza from './components/Pizza.vue'
+
+  export default {
+    name: 'app',
+    data(){
+      return {
+        pizzaName: 'Пицца от школы программистов',
+        pizzaPrice: 600,
+        imgURL: "https://w-dog.ru/wallpapers/5/5/473285374806349/eda-vkusno-picca-produkty-pitaniya-krasnyj-fon.jpg"
+      }
+    },
+    components: {
+      appPizza: Pizza
+    } 
+  }
+</script>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  .components_list {
+    width: fit-content;
+    margin: auto;
+    text-align: left;
+  }
+</style>
+
---
+++ pizza/src/components/Pizza.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h2> Pizza Name: {{pizzaName}}</h2>
+        <p>Price: {{pizzaPrice}}</p>    
+        <div class="pizza_img" :style="{backgroundImage: 'url(' + imgURL + ')'}"></div>
+
+        <div>
+            <slot name="components"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['pizzaName','pizzaPrice', 'imgURL'],    
+    }
+</script>
+
+<style>
+    .pizza_img {
+        width: 400px;
+        height: 300px;
+        margin: auto;
+        background-size: cover;
+    }
+</style>
