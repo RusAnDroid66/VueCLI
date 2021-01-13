b4043ef6d92f772f830300abe617aeb016014b29
Arkhangelsky Andrey <an.anarxist@mail.ru>
2021-01-14 02:10:02 +0300

    Added app with facts about cats

---
+++ cat_facts/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <CatFact>
+      {{ fact_text }}
+    </CatFact>
+  </div>
+</template>
+
+<script>
+import CatFact from './components/CatFact.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CatFact
+  },
+
+  data() {
+    return {
+      fact_text: "",
+    }
+  },
+
+  methods: {
+    get_fact() {
+      this.axios.get("http://37.77.104.246/api/random-fact.php")
+        .then((response) => {
+          this.fact_text = response.data;  
+        }
+      );
+    }
+  },
+
+  mounted() {
+    this.get_fact()
+    setInterval(() => this.get_fact(), 10000);
+  }
+}
+</script>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ cat_facts/src/components/CatFact.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <h1>Случайный факт о кошках:</h1>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'CatFact'
+    }
+</script>
+
+<style scoped>
+    .container {
+        width: 764px;
+        margin: auto;
+        box-shadow: 0 0 15px 0 #5f5f5f73;
+        padding: 50px;
+    }
+    h1 {
+        margin: 0;
+        margin-bottom: 25px;
+    }
+</style>
